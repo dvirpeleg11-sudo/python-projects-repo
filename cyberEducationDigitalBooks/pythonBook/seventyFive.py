2276869d26e6c72ea63788bb65d534b77ca6f82b
dvirpeleg11-sudo <dvirpeleg11@gmail.com>
2026-06-12 17:21:53 +0300

    deleted xigool empty folder.

--- cyberEducationDigitalBooks/pythonBook/seventyFive.py
+++ cyberEducationDigitalBooks/pythonBook/seventyFive.py
@@ -1,5 +1,18 @@
 def factorial(number: int) -> int:
     res = 1
     if number > 2:
-        for i in range(number, 2, -1):
+        for i in range(2, number + 1):
             res *= i
+        return res
+    elif number > 0:
+        return number
+    else:
+        print("cant do to - values.")
+
+
+
+def main():
+    print(factorial(-1))
+
+if __name__ == '__main__':
+    main()
